b5f1f5425d51102ffd36535f132ff30f6a6cd70c
Raymond Oluoch <rasholuoch@gmail.com>
2019-09-09 19:35:45 +0300

    user interface getting user input with chomp

--- lib/board.rb
+++ lib/board.rb
@@ -2,26 +2,5 @@
 # frozen_string_literal: true
 
 class Board
-  include UserInterface
-  attr_accessor :board
 
-  def initialize
-    @board = Array.new(9)
-  end
-
-  def display_init_board
-    row = [
-      '   ' \
-        '|' \
-        '   ' \
-        '|' \
-        '   '
-    ]
-    separator = '-----------'
-    puts row
-    puts separator
-    puts row
-    puts separator
-    puts row
-  end
 end
